04b79f67f990783316821da29331619cd2d33a9a
nxthxnael <nathanael.mutua.m@gmail.com>
2026-01-17 09:42:34 +0300

    feat: add onboarding welcome page - add dynamic onboarding navigation and styling - add lottie package to pubspec.yaml - add student.json lottie animation file to assets/lottie - add dummy page to redirect to on clicking get started - add content to intro_page_1.dart

--- learning_app/lib/pages/onboarding_screen.dart
+++ learning_app/lib/pages/onboarding_screen.dart
@@ -7,6 +7,7 @@ import 'package:learning_app/utils/intro_screens/intro_page_1.dart';
 import 'package:learning_app/utils/intro_screens/intro_page_2.dart';
 import 'package:learning_app/utils/intro_screens/intro_page_3.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:learning_app/pages/home_page.dart';
 
 /// Onboarding screen widget
 class OnboardingScreen extends StatefulWidget {
@@ -21,6 +22,9 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   /// Controller for managing page view navigation
   final PageController _controller = PageController();
 
+  /// boolean to keep track of if we are on the last page
+  bool onLastPage = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +32,11 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         children: [
           PageView(
             controller: _controller,
+            onPageChanged: (index) {
+              setState(() {
+                onLastPage = (index == 2);
+              });
+            },
             children: [
               // Individual onboarding pages
               IntroPage1(),
@@ -42,17 +51,94 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             left: 0,
             right: 0,
             child: Center(
-              child: SmoothPageIndicator(
-                controller: _controller,
-                count: 3, // Matches number of pages
-                effect: const WormEffect(
-                  activeDotColor: Colors.white,
-                  dotColor: Colors.white54,
-                  dotHeight: 12,
-                  dotWidth: 12,
-                  spacing: 8,
-                  type: WormType.normal,
-                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  if (!onLastPage) ...[
+                    // what will appear on all the onboarding screens apart from the last one
+                    // skip
+                    GestureDetector(
+                      onTap: () {
+                        _controller.jumpToPage(2);
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 8,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.blue.shade100, // Background color
+                          borderRadius: BorderRadius.circular(
+                            20,
+                          ), // Rounded corners
+                        ),
+                        child: const Text(
+                          'Skip',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0xFF0D47A1),
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // dot indicator
+                    SmoothPageIndicator(
+                      controller: _controller,
+                      count: 3, // Matches number of pages
+                      effect: const WormEffect(
+                        activeDotColor: Color(0xFF0D47A1),
+                        dotColor: Color.fromRGBO(91, 91, 91, 0.685),
+                        dotHeight: 12,
+                        dotWidth: 12,
+                        spacing: 8,
+                        type: WormType.normal,
+                      ),
+                    ),
+
+                    // next button
+                    GestureDetector(
+                      onTap: () {
+                        _controller.nextPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeIn,
+                        );
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 8,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.blue.shade100, // Background color
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: const Text(
+                          'Next',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0xFF0D47A1),
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ] else ...[
+                    /// what will appear on the last onboarding screen
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        );
+                      },
+                      child: Text('get started'),
+                    ),
+                  ],
+                ],
               ),
             ),
           ),
