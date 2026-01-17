f88dc39ac2df3bdfe94c38a954bdabf04606e073
nxthxnael <nathanael.mutua.m@gmail.com>
2026-01-17 09:42:34 +0300

    feat: add onboarding welcome page - add dynamic onboarding navigation and styling - add lottie package to pubspec.yaml - add student.json lottie animation file to assets/lottie - add dummy page to redirect to on clicking get started - add content to intro_page_1.dart

--- learning_app/lib/utils/intro_screens/intro_page_2.dart
+++ learning_app/lib/utils/intro_screens/intro_page_2.dart
@@ -6,14 +6,32 @@ class IntroPage2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.pink,
-      child: Center(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [
+            const Color.fromARGB(255, 3, 49, 142),
+            const Color.fromARGB(255, 8, 114, 184),
+            const Color.fromARGB(255, 25, 66, 230),
+          ],
+          stops: const [0.0, 0.5, 1.0],
+        ),
+      ),
+      child: const Center(
         child: Text(
           "Features",
-          style: const TextStyle(
+          style: TextStyle(
             fontSize: 32,
             color: Colors.white,
             fontWeight: FontWeight.bold,
+            shadows: [
+              Shadow(
+                blurRadius: 4,
+                color: Colors.black54,
+                offset: Offset(2, 2),
+              ),
+            ],
           ),
         ),
       ),
